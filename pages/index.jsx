532eb8d30c2954508a3d593d2bf2fe5fb995c724
Kuanying, Li (李冠穎) <45960985+kuan0426@users.noreply.github.com>
2023-12-20 02:54:50 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import InstructionsComponent from "../components/InstructionsComponent";
 export default function Home() {
   return (
     <div>
+      < link rel="icon" href="https://www.webdesigns.com.tw/images/logo.ico" type="image/x-icon" / >
       <main className={styles.main}>
         <InstructionsComponent>123456</InstructionsComponent>
         Hi

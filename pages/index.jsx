badabe8d1fdb0b06bfd8b437b6cc9a6bf0734bdd
Kuanying, Li (李冠穎) <45960985+kuan0426@users.noreply.github.com>
2023-12-20 02:49:07 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div>
       <main className={styles.main}>
         <InstructionsComponent></InstructionsComponent>
+        Hi
       </main>
     </div>
   );

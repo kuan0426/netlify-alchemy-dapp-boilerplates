826d4742b020630a360e9e2b7f2b623ac948b35f
Kuanying, Li (李冠穎) <45960985+kuan0426@users.noreply.github.com>
2024-06-01 16:12:08 +0800

    Update mainLayout.jsx

--- layout/mainLayout.jsx
+++ layout/mainLayout.jsx
@@ -3,7 +3,9 @@ import Navbar from "../components/navigation/navbar";
 export default function MainLayout({ children }) {
 	return (
 		<div>
+			test
             <Navbar />
+			testtest
             {children}
 		</div>
 	);

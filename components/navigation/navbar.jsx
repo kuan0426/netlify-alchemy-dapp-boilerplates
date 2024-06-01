58c8a4007807aeceb7b0e53dcc404c6da749b9cd
Kuanying, Li (李冠穎) <45960985+kuan0426@users.noreply.github.com>
2024-06-01 15:52:57 +0800

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -4,6 +4,9 @@ export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+				<img className={styles.alchemy_logo} src="https://i.seadn.io/gae/ZFwSEkFGjvZUF6H51whQekBEi7tgxgh42WwNcCHTFYFsWH35P6iueCinhZzN_GuD5EBG6nXkvC13ADPvN9T6zR7OEQJ_tb0Q2J4?auto=format&dpr=1&w=85"></img>
+			</a>
+			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
 				<img className={styles.alchemy_logo} src="https://i.seadn.io/gae/ZFwSEkFGjvZUF6H51whQekBEi7tgxgh42WwNcCHTFYFsWH35P6iueCinhZzN_GuD5EBG6nXkvC13ADPvN9T6zR7OEQJ_tb0Q2J4?auto=format&dpr=1&w=90"></img>
 			</a>
 			<ConnectButton label="Sign in"></ConnectButton>

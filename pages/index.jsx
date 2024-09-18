3ed1428bc8af3141dca8013dbc323a33559465eb
Guanny <a1367577@gmail.com>
2024-09-18 15:46:04 +0800

    Update index.jsx
    2nd Git test

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 import styles from "../styles/Home.module.css";
 import InstructionsComponent from "../components/InstructionsComponent";
 
-<h1>歡迎來到我的 DApp</h1>
+
 
 
 export default function Home() {
   return (
     <div>
+      <h1>歡迎來到我的 DApp</h1>
+      <p>這是首頁的介紹段落。</p>
       < link rel="icon" href="https://i.seadn.io/gae/ZFwSEkFGjvZUF6H51whQekBEi7tgxgh42WwNcCHTFYFsWH35P6iueCinhZzN_GuD5EBG6nXkvC13ADPvN9T6zR7OEQJ_tb0Q2J4?auto=format&dpr=1&w=90" type="image/x-icon" / >
       <main className={styles.main}>
         <InstructionsComponent>123456</InstructionsComponent>
